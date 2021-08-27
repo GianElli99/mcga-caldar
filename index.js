717424e23aea70cac9456acd62c6840ed778b3cd
GianElli99 <gianelli99@hotmail.com>
2021-08-27 10:55:19 -0300

    Configuración inicial del servidor y prettier

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hi');
+const { json } = require('body-parser');
+const express = require('express');
+const app = express();
+const PORT = 8090;
+
+app.use(json());
+
+app.get('/', (req, res) => {
+  res.send('Hi!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
