822dcdefd5ddb3baf20a1683b504ee3134ff6a18
GianElli99 <gianelli99@hotmail.com>
2021-09-01 11:46:38 -0300

    estructura basica de constructoras añadida

---
+++ controladoras/constructoras.controladoras.js
@@ -0,0 +1,164 @@
+const { request, response } = require('express');
+const fs = require('fs');
+
+const obtenerConstructoras = (req = request, res = response) => {
+  try {
+    const { es_particular, ciudad } = req.query;
+
+    let edificios = listarEdificios();
+
+    if (
+      es_particular &&
+      (es_particular === 'true' || es_particular === 'false')
+    ) {
+      let particularBooleano = es_particular === 'true';
+      edificios = edificios.filter(
+        (edif) => edif.es_particular === particularBooleano
+      );
+    }
+
+    if (ciudad) {
+      edificios = edificios.filter((edif) =>
+        edif.ciudad.toLowerCase().includes(ciudad.toLowerCase())
+      );
+    }
+
+    res.send(edificios);
+  } catch (error) {
+    res.status(500).json({ error: 'Un error ha ocurrido' });
+  }
+};
+
+const obtenerConstructora = (req = request, res = response) => {
+  try {
+    const edificioId = parseInt(req.params.id);
+    const edificio = listarEdificios().find(
+      (edificio) => edificio.id === edificioId
+    );
+
+    if (edificio) {
+      res.json(edificio);
+    } else {
+      res.json({});
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Un error ha ocurrido' });
+  }
+};
+
+const obtenerEdificios = (req = request, res = response) => {};
+
+const agregarConstructora = (req = request, res = response) => {
+  try {
+    const {
+      direccion = '',
+      ciudad = '',
+      codigo_postal = '',
+      es_particular = true,
+      contacto = '',
+    } = req.body;
+    const id = Math.round(Math.random() * 1000);
+
+    const nuevoEdificio = {
+      id,
+      direccion,
+      ciudad,
+      codigo_postal,
+      es_particular,
+      contacto,
+    }; //TODO: necesita refactorizacion, crear clase edificio
+    const edificios = listarEdificios();
+    edificios.push(nuevoEdificio);
+    guardarEdificios(edificios);
+
+    res.json(nuevoEdificio);
+  } catch (error) {
+    res.status(500).json({ error: 'Un error ha ocurrido' });
+  }
+};
+
+const eliminarConstructora = (req = request, res = response) => {
+  try {
+    const edificioId = parseInt(req.params.id);
+    let edificios = listarEdificios();
+
+    const edificioAEliminar = edificios.find(
+      (edificio) => edificio.id === edificioId
+    );
+
+    if (edificioAEliminar) {
+      edificios = edificios.filter(
+        (edificio) => edificio !== edificioAEliminar
+      );
+      guardarEdificios(edificios);
+
+      res.json(edificioAEliminar);
+    } else {
+      res.json({});
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Un error ha ocurrido' });
+  }
+};
+
+const modificarConstructora = (req = request, res = response) => {
+  try {
+    const id = parseInt(req.params.id);
+    const {
+      direccion = '',
+      ciudad = '',
+      codigo_postal = '',
+      es_particular = true,
+      contacto = '',
+    } = req.body;
+
+    const edificioModificado = {
+      id,
+      direccion,
+      ciudad,
+      codigo_postal,
+      es_particular,
+      contacto,
+    }; //TODO: necesita refactorizacion, crear clase edificio
+    let edificios = listarEdificios();
+    let modificacionRealizada = false;
+    for (let i = 0; i < edificios.length; i++) {
+      if (edificioModificado.id === edificios[i].id) {
+        edificios[i] = edificioModificado;
+        modificacionRealizada = true;
+        break;
+      }
+    }
+    if (modificacionRealizada) {
+      guardarEdificios(edificios);
+      res.json(edificioModificado);
+    } else {
+      res.json({});
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Un error ha ocurrido' });
+  }
+};
+
+const listarConstructoras = () => {
+  let datosCrudos = fs.readFileSync('./datos/constructoras.json');
+  let edificios = JSON.parse(datosCrudos);
+
+  return edificios;
+};
+
+const guardarConstructoras = (edificios) => {
+  fs.writeFileSync(
+    './datos/constructoras.json',
+    JSON.stringify(edificios, null, 2)
+  );
+};
+
+module.exports = {
+  obtenerConstructoras,
+  obtenerConstructora,
+  agregarConstructora,
+  eliminarConstructora,
+  modificarConstructora,
+  obtenerEdificios,
+};
--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const { json } = require('body-parser');
 
 const routerCalderas = require('./rutas/calderas.rutas');
 const routerEdificios = require('./rutas/edificios.rutas');
+const routerConstructoras = require('./rutas/constructoras.rutas');
 
-const PORT = 8090;
+const port = process.env.PORT || 8090;
 const app = express();
 
 app.use(json());
 
 app.use('/calderas', routerCalderas);
 app.use('/edificios', routerEdificios);
+app.use('/constructoras', routerConstructoras);
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
